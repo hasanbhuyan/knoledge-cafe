ce6aa5012f794bc4c437179c2696099102de4741
hasanbhuyan <muhammadhasanbhuyan.com>
2024-10-23 18:47:20 +0600

    knoledge cafe

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      
+      <h1 className='text-6xl text-center bg-green-500 py-3'>Knoledge Cafe</h1>
+    </>
+  )
+}
+
+export default App
